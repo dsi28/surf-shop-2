fa91c49b66edfedf56054b619886cba6240cc310
David <dsi28@hotmail.com>
2019-04-30 14:15:22 -0400

    set up post controller

---
+++ controllers/posts.js
@@ -0,0 +1,27 @@
+const Post = require('../models/post');
+
+module.exports = {
+    postsIndex(req,res,next){
+
+    },
+    postsNew(req,res,next){
+
+    },
+    postsCreate(req,res,next){
+
+    },
+    postsEdit(req,res,next){
+
+    },
+    postsUpdate(req,res,next){
+
+    },
+    postsShow(req,res,next){
+
+    },
+    postsDelete(req,res,next){
+
+    }
+
+
+};
